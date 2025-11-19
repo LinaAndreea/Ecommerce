7e0612cd92d78f34e2cc23264852735147fc6867
itapalaga <57479932+itapalaga@users.noreply.github.com>
2025-11-19 14:16:51 +0200

    fix-for-configService

--- services/ConfigService.js
+++ services/ConfigService.js
@@ -27,8 +27,6 @@ class ConfigService {
             },
              carousel: {
                 container: '.swiper-container, .carousel, .slider, [class*="carousel"]',
-                slides: '.swiper-slide, .carousel-slide, .slide, [class*="slide"]',
-                container: '.swiper-container, .carousel, .slider, [class*="carousel"]',
                // Added .carousel-item to match the markup on the site
                 slides: '.swiper-slide, .carousel-slide, .carousel-item, .slide, [class*="slide"]',
                 featuredProducts: '[data-id="218380"], .featured-products .carousel, .product-carousel'
